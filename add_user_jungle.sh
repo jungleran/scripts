8c94dc4047922ba3aea01da6d87b0ecefe7599ec
Jungle Ran <jungle@ranqiangjun.com>
2017-12-25 16:13:45 +0800

    Create add_user_jungle.sh

---
+++ add_user_jungle.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+adduser jungle
+usermod -aG sudo jungle
+mkdir -p /home/jungle/.ssh
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4W9ib34VKwnK6kGoIcONtplH6PR1ZqvhD31LdRyI6Alf8SiWoqiPBypNxL6k4FO6MBtKoTA0Vnv6E9DfjS8WqRg4qRhglWkshxJPmp+39htYXuj3XRpfRTgiupjpAev3TOI8qrYRnnmD1ojd9NjT/N8DOJmpfAlmG2HwA0v3myDAMWd01BNVB1ZRbZIJ0jrr/uN+6tMLxs4pRgblIlAabRF9nnopYczVsQNPJEiGefRDBpiE5pILZnoLQZF4Yq8JSUm335Fnez9M5AC3hp/VUPkXS+G1PBlgXaVMo6uHZzV2Db98DA1IBE2x1Cx4yhS1IxvzHgsHdBdet0TVSZGOz jungle@ranqiangjun.com' >> /home/jungle/.ssh/authorized_keys
+chown jungle:jungle /home/jungle/.ssh/authorized_keys
+chown jungle:jungle /home/jungle/.ssh
+chmod 600 /home/jungle/.ssh/authorized_keys
+chmod 700 /home/jungle/.ssh
+echo "jungle ALL=NOPASSWD: ALL" >> /etc/sudoers
